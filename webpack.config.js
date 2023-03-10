1edc2c2ebc4a4885d96aaf78d16e9f8fe6ef3d04
Vladimir horev <Vshorev@yandex.ru>
2023-03-10 15:23:12 +0700

    1

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
+
+    devServer: {
+        contentBase: path.join(__dirname,'dist'),
+        port:3003
+    },
+
     entry: '/main.js',
     module: {
         rules: [
@@ -16,10 +22,12 @@ module.exports = {
             { test: /\.(js)$/, use: 'babel-loader' }
         ]
     },
+
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main_bundle.js'
-    }
+    },
+
 }
 
 
